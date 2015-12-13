b93b7b0d1cc1442ea24db178f839e373bad655c7
calvintan <Calvin@calvin.gateway.2wire.net>
2015-12-13 21:42:09 +0800

    Fixed the 'scoring per round' mechanics

--- games.js
+++ games.js
@@ -21,14 +21,13 @@ Meteor.methods({
     Games.insert(game)
   },
   takeTurn: function (gameId, id, card) {
-    // console.log(id, card)
     var game = Games.findOne(gameId)
     var choices = game.players[id].choices
-    //var hand = game.players[id].hand
 
-console.log(game.round)
-console.log(game.players[id])
-
+    console.log('the current round: ', game.round)
+    console.log('current players id: ', game.players[id])
+    console.log(game.currentTurn);
+    console.log(Meteor.userId());
     // if (game.currentTurn[0] === id && Turns.inHand(hand, card))
     // if not the current user's turn OR if the player has no moves left for the round
     // if (game.currentTurn[0] !== id || !move > 0) return
@@ -45,60 +44,40 @@ console.log(game.players[id])
       game.currentTurn.unshift(game.currentTurn.pop())
       game.moves = 2 // reset no of moves
 
-      if (game.moves % 2 === 0) {
+      if (game.round % 2 === 0) {
         // tally the score
         var mePlayer = game.players[game.currentTurn[0]]
         var otherPlayer = game.players[game.currentTurn[1]]
-
-        // console.log(mePlayer.choices);
-        // console.log(otherPlayer.choices);
-
-        function scoringRound () {
-          if (mePlayer.choices[1] == otherPlayer.choices[0]) {
-            mePlayer.score += 1
-            console.log('current player', mePlayer.score);
-          } else {
-            mePlayer.score += 0
-            console.log('current player', mePlayer.score);
-          }
-
-          if (otherPlayer.choices[1] == mePlayer.choices[0]) {
-            otherPlayer.score += 1
-            console.log('opponent', otherPlayer.score);
-
-          } else {
-            otherPlayer.score += 0
-            console.log('opponent', otherPlayer.score);
-          }
-        }
 
         function resetChoices() {
           mePlayer.choices = []
           otherPlayer.choices = []
         }
 
+        function scoringRound () {
+          if (mePlayer.choices[1] == otherPlayer.choices[0]) {
+            mePlayer.score += 1
+            console.log('i scored: ', mePlayer.score);
+          } else {
+            mePlayer.score += 0
+            console.log('i didnt score: ', mePlayer.score);
+          }
+
+          if (otherPlayer.choices[1] == mePlayer.choices[0]) {
+            otherPlayer.score += 1
+            console.log('opponent scored: ', otherPlayer.score);
+
+          } else {
+            otherPlayer.score += 0
+            console.log('opponent didnt score:', otherPlayer.score);
+          }
+        }
         scoringRound()
-        // function scoreTheRound(options, callback) {
-        //   callback()
-        // }
-        //
-        // scoreTheRound(scoringRound, resetChoices){
-        //   console.log('before callback me', mePlayer.choices);
-        //   console.log('before callback opponent', otherPlayer.choices);
-        //
-        //   // resetChoices()
-        //
-        //   console.log('after callback me', mePlayer.choices);
-        //   console.log('after callback opponent', otherPlayer.choices);
-        // })
-
+        resetChoices()
       } else {
-        // not 2 rounds yet, no scoring done
-        return
+        // not 2 rounds yet, no scoring will be done
       }
-
       game.round = game.round + 1
-
     } else {
       console.log('if game.moves == 0', game.moves);
       return
@@ -109,7 +88,7 @@ console.log(game.players[id])
     // game.currentTurn.unshift(game.currentTurn.pop())
 
     if (game.round >= 20) {
-      console.log('yay!')
+      // console.log('yay!')
       // scoreGame(game) // score the game
     }
     Games.update(gameId, game)
